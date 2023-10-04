e6c776bb9e1908d3876a7da188d4c9940336e749
PAVAN KUMAR P S <64530269+pavanscienti@users.noreply.github.com>
2023-10-04 11:34:44 +0530

    Update index.js
    URL Settings

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/auth", userRouter)
 app.use("/recipes",recipeRouter)
 
-mongoose.connect("mongodb+srv://pvnkumarps:pavan@cluster0.thzbwzz.mongodb.net/?retryWrites=true&w=majority",
+mongoose.connect("YOUR MONGODB URL FOR CONNECTION",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
